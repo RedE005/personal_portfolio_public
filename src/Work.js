55f3b29d3338c3b5dad6e789cc42f18ca8282d41
Shiva Ram Reddy Bethi <iamreddy005@gmail.com>
2024-08-30 14:08:19 -0700

    Added AWS to work history and added GnuPG for verified commits

--- src/Work.js
+++ src/Work.js
@@ -1,10 +1,46 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
 import ServiceNow from "./ServiceNow.png";
 import NCR from "./NCRCorporation.png";
+import AWS from "./AWS.png";
 
 function Work() {
   return (
     <div className="container-9 w-container">
+      <section
+        className="hero-heading-right"
+      >
+        <div className="container-10">
+          <div className="hero-wrapper">
+            <div className="hero-split">
+              <img
+                src={AWS}
+                loading="lazy"
+                sizes="100vw"
+                srcset={AWS}
+                alt="Photo of AWS"
+                className="shadow-two"
+              />
+            </div>
+            <div className="hero-split">
+              <h1 className="heading-40"><span className="text-span-21">Software Development Engineer Intern</span></h1>
+              <div>
+                <strong><span className="text-span-22">May'24 - August'24</span></strong>
+              </div>
+              <p className="margin-bottom-24px">
+                Engineered cross-account log vending system using AWS TypeScript CDK and Java SDK,
+                integrating Lambda, S3, SQS, CloudWatch Logs to process numerous messages per minute
+                and handle large log files in size. Implemented a scalable serverless architecture
+                capable of managing logs across different customer accounts simultaneously, utilizing
+                STS for secure cross-account access and achieving high log processing success rate.
+                Developed an efficient log chunking algorithm that optimized CloudWatch API usage,
+                reducing API calls and cost. Designed a fault-tolerant system with automatic retries
+                and error handling, maximizing uptime.
+                <br />
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
       <section
         className="hero-heading-right"
       >
@@ -23,7 +59,7 @@ function Work() {
             <div className="hero-split">
               <h1 className="heading-40"><span className="text-span-21">Software Engineer II</span></h1>
               <div>
-                <strong><span className="text-span-22">Mar'23 - Aug'23</span></strong>
+                <strong><span className="text-span-22">March'23 - August'23</span></strong>
               </div>
               <p className="margin-bottom-24px">
                 Worked in Strategic Portfolio Management Engineering team,
@@ -55,32 +91,32 @@ function Work() {
               />
             </div>
             <div className="hero-split">
-                  <h1 className="heading-40"><span className="text-span-21">Software Engineer I</span></h1>
-                  <div>
-                    <strong><span className="text-span-22">Oct'20 - Feb'23</span></strong>
-                  </div>
-                  <p className="margin-bottom-24px">
-                    Worked in core development of ATM software product (Activate
-                    Enterprise) used by many multi-national banks to handle the
-                    flow of transactions at back-end of an ATM. Developed APIs
-                    to revive the health of an ATM using .Net Framework, C# and
-                    Jenkins. Developed a Windows Forms App using .Net Framework
-                    which is used to log the API calls made in the transaction
-                    flow.
-                    <br />
-                  </p>
-                  <h1 className="heading-41"><span className="text-span-21">Software Engineer Intern</span></h1>
-                  <div>
-                    <strong><span className="text-span-22">Jan'20 - Oct'20</span></strong>
-                  </div>
-                  <p className="margin-bottom-24px">
-                    Worked on an internal tool called XFS Simulator which helps
-                    in simulating the ATM application on development systems.
-                    Used Java, SpringBoot to work on the internal tool.
-                    Performed automation of the transactional test cases through
-                    Robot Framework, Selenium Library and AutoIT tool.
-                    <br />
-                  </p>
+              <h1 className="heading-40"><span className="text-span-21">Software Engineer I</span></h1>
+              <div>
+                <strong><span className="text-span-22">October'20 - February'23</span></strong>
+              </div>
+              <p className="margin-bottom-24px">
+                Worked in core development of ATM software product (Activate
+                Enterprise) used by many multi-national banks to handle the
+                flow of transactions at back-end of an ATM. Developed APIs
+                to revive the health of an ATM using .Net Framework, C# and
+                Jenkins. Developed a Windows Forms App using .Net Framework
+                which is used to log the API calls made in the transaction
+                flow.
+                <br />
+              </p>
+              <h1 className="heading-41"><span className="text-span-21">Software Engineer Intern</span></h1>
+              <div>
+                <strong><span className="text-span-22">January'20 - October'20</span></strong>
+              </div>
+              <p className="margin-bottom-24px">
+                Worked on an internal tool called XFS Simulator which helps
+                in simulating the ATM application on development systems.
+                Used Java, SpringBoot to work on the internal tool.
+                Performed automation of the transactional test cases through
+                Robot Framework, Selenium Library and AutoIT tool.
+                <br />
+              </p>
             </div>
           </div>
         </div>
